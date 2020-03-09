c45f6ca8cdfa9336ccd4cb14d0edfd5aeb83b635
Kiril <irodin007@gmail.com>
2020-03-09 18:10:38 +0300

    classes added

--- Class_training/Program.cs
+++ Class_training/Program.cs
@@ -2,11 +2,77 @@
 
 namespace Class_training
 {
-    class Program
-    {
-        static void Main()
-        {
-            
-        }
-    }
+	class Diploma
+	{
+		string orgName;
+		public string OrgName
+		{
+			get
+			{
+				return orgName;
+			}
+			set
+			{
+				orgName = value;
+			}
+		}
+
+		string qualifications;
+		public string Qualifications
+		{
+			get
+			{
+				return qualifications;
+			}
+			set
+			{
+				qualifications = value;
+			}
+		}
+
+		DateTime diplomeDateTime;
+		public DateTime DiplomeDateTime
+		{
+			get
+			{
+				return diplomeDateTime;
+			}
+			set
+			{
+				diplomeDateTime = value;
+			}
+		}
+
+		public Diploma(string orgName, string qualifications, DateTime diplomeDateTime)
+		{
+			this.orgName = orgName;
+			this.qualifications = qualifications;
+			this.diplomeDateTime = diplomeDateTime;
+		}
+
+		public Diploma()
+		{
+			orgName = "DNU";
+			qualifications = "Tester";
+			diplomeDateTime = DateTime.Now;
+		}
+	}
+
+	class Doctor
+	{
+		Person()
+		{
+
+		}
+	}
+
+	class Program
+	{
+		static void Main()
+		{
+			
+		}
+	}
+
+	enum Category { High, First, Second }
 }
